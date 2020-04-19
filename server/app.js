97d121495980e50271e2d88e62a7501bbc9c3f3a
GustafWallstrom <46966247+GustafWallstrom@users.noreply.github.com>
2020-04-19 08:37:03 +0200

    Some progress on the login API, still can't get the server to validate the user with mongoose

--- server/app.js
+++ server/app.js
@@ -1,22 +1,40 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
-const url = "mongodb://localhost/discore";
+const url = 'mongodb://localhost:27017/user';
+
+const User = require('./model/user');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.post("api/user/login", (req, res) => {
-  mongoose.connect(url, function (err) {
+app.post('/api/user/login', (req, res) => {
+  mongoose.connect(url, {
+    useMongoClient: true
+  }, function (err) {
     if (err) throw err;
-    console.log(
-      "connected successfully, username is ",
-      req.body.username,
-      " password is ",
-      req.body.password
-    );
+    User.find({
+      username: req.body.username,
+      password: req.body.password
+    }, function (err, user) {
+      if (err) throw err;
+      if (user.length === 1) {
+        return res.status(200).json({
+          status: 'success',
+          data: user
+        })
+      } else {
+        return res.status(200).json({
+          status: 'fail',
+          message: 'Login Failed'
+        })
+      }
+
+    })
   });
-});
+})
 
 app.listen(3000, () => console.log("Blog server running on port 3000!"));
