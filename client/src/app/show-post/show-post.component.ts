16945f514d00e0e918037dffd8ac1bcbede1aca2
GustafWallstrom <46966247+GustafWallstrom@users.noreply.github.com>
2020-04-28 23:41:52 +0200

    Added component 'show-post' to show posts from database for user who are logged in

---
+++ client/src/app/show-post/show-post.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ShowPostService } from './show-post.service';
+import { Post } from '../models/post.model';
+
+@Component({
+  selector: 'app-show-post',
+  templateUrl: './show-post.component.html',
+  providers: [ ShowPostService ]
+})
+export class ShowPostComponent implements OnInit {
+
+  public posts : any [];
+
+  constructor(private showPostService: ShowPostService) {
+  	
+  }
+
+  ngOnInit(){
+  	this.getAllPost();
+  }
+
+
+
+  getAllPost(){
+  	this.showPostService.getAllPost().subscribe(result => {
+      this.posts = result['data'];
+  	});
+  }
+
+}
